1cbc47b75ca82e6257910d1e7e4a1307b556958f
Thea Aarrestad <thea.aarrestad@cern.ch>
2018-01-25 16:16:02 +0100

    added latest CP scripts

--- VVResonances/interactive/do-control-plots.py
+++ VVResonances/interactive/do-control-plots.py
@@ -11,6 +11,9 @@ import os
 from array import array
 from time import sleep
 
+from datetime import datetime
+startTime = datetime.now()
+
 ROOT.gROOT.SetBatch(True)
 
 
@@ -112,7 +115,7 @@ def getPlotters(samples,isData=False,corr="1"):
                     
     return  plotters
 
-def compare(p1,p2,var,postfix,cut1,cut2,bins,mini,maxi,title,unit,leg1,leg2,logY=0):
+def compareSigQCD(p1,p2,var,postfix,cut1,cut2,bins,mini,maxi,title,unit,leg1,leg2,logY=0):
     T = 0.08*H_ref
     B = 0.12*H_ref 
     L = 0.12*W_ref
@@ -244,6 +247,75 @@ def compareQCD(p1,p2,p3,var,postfix,cut,bins,mini,maxi,title,unit,leg1,leg2,leg3
 
     return canvas,h1,h2,h3,legend #,pt
 
+def dataMC(data,mc,sig,var,postfix,cut,bins,mini,maxi,title,unit,leg1,leg2,leg3,logY=0):
+    T = 0.08*H_ref
+    B = 0.12*H_ref 
+    L = 0.12*W_ref
+    R = 0.04*W_ref
+    name = "%s_%s" %(postfix, var.replace("(","").replace(")","").replace("-","_"))
+    canvas = ROOT.TCanvas(name,name,50,50,W,H)
+    canvas.SetFillColor(0)
+    canvas.SetBorderMode(0)
+    canvas.SetFrameFillStyle(0)
+    canvas.SetFrameBorderMode(0)
+    canvas.SetLeftMargin( L/W )
+    canvas.SetRightMargin( R/W )
+    canvas.SetTopMargin( T/H )
+    canvas.SetBottomMargin( B/H )
+    canvas.SetTickx(0)
+    canvas.SetTicky(0)
+    
+    ROOT.gStyle.SetOptStat(0)
+    ROOT.gStyle.SetOptTitle(0)
+    canvas.cd()
+    legend = ROOT.TLegend(0.62,0.7,0.92,0.9,"","brNDC")
+    legend.SetBorderSize(0)
+    legend.SetLineColor(1)
+    legend.SetLineStyle(1)
+    legend.SetLineWidth(1)
+    legend.SetFillColor(0)
+    legend.SetFillStyle(0)
+    legend.SetTextFont(42)
+    
+    hData=data.drawTH1(var,cut,lumi,bins,mini,maxi,title,unit,"HIST")
+    hMC=mc.drawTH1(var,cut,lumi,bins,mini,maxi,title,unit,"HIST")
+    hSig=sig.drawTH1(var,cut,lumi,bins,mini,maxi,title,unit,"HIST")
+    hSig.Scale((hMC.Integral()/hSig.Integral()))
+    
+    hData.Draw("EP")
+    hMC.Draw("HISTCSAME")
+    hSig.Draw("HISTCSAME")
+    maxY = hData.GetMaximum()*1.5
+    if hData.GetMaximum() < hMC.GetMaximum(): 
+		 maxY = hMC.GetMaximum()*1.5
+    if logY: 
+		 canvas.SetLogy()
+		 maxY = maxY*200
+    hData.GetYaxis().SetRangeUser(0.001,maxY)
+    # hData.DrawNormalized("HISTC")
+    # hMC.DrawNormalized("SAMEHIST")
+    # hData=data.drawTH1Binned(var,cut1,lumi,bins,title,unit, "HISTC")
+    # hMC=mc.drawTH1Binned(var,cut2,lumi,bins,title,unit, "SAMEHIST")
+    legend.AddEntry(hData,leg1,"LEP")
+    legend.AddEntry(hMC,leg2,"F")
+    legend.AddEntry(hSig,leg3,"F")
+    legend.Draw()
+    
+    
+    # pt =ROOT.TPaveText(0.1577181,0.9562937,0.9580537,0.9947552,"brNDC")
+    # pt.SetBorderSize(0)
+    # pt.SetTextAlign(12)
+    # pt.SetFillStyle(0)
+    # pt.SetTextFont(42)
+    # pt.SetTextSize(0.03)
+    #
+    # pt.Draw()
+    cmslabel_prelim(canvas,'2016',11)
+
+    
+    return canvas,hData,hMC,legend #,pt
+	
+
 
 
 # QCDPtPlotters = getPlotters(nonResTemplate,False)
@@ -260,8 +332,7 @@ def compareQCD(p1,p2,p3,var,postfix,cut,bins,mini,maxi,title,unit,leg1,leg2,leg3
 
 QCDherwigPlotters = getPlotters('QCD_Pt-',False)
 QCDherwig = MergedPlotter(QCDherwigPlotters)
-QCDherwig.setLineProperties(1,434,2)
-QCDherwig.setFillProperties(1001,0)
+QCDherwig.setFillProperties(1001,921)
 
 DATAPlotters = getPlotters(dataTemplate,True)
 data=MergedPlotter(DATAPlotters)
@@ -282,21 +353,27 @@ for p in purities:
 	print "Plotting variables for category %s" %(p)
 	cut='*'.join([cuts['common'],cuts[p]])
 	postfix = p
-	c1 = compare(QCDherwig,data,'jj_l1_softDrop_mass',postfix,cut,cut,80,55.,215.,'V candidate softdrop mass (GeV)',"","QCD","Jet  HT")
+	
+	c1 = dataMC(QCDherwig,QCDherwig,sig,'jj_l1_softDrop_mass',postfix,cut,40,55.,215.,'V candidate softdrop mass',"GeV","QCD","Data","G_{Bulk}(2 TeV)#rightarrowWW")
 	c1[0].SaveAs(directory+"/"+c1[0].GetName()+".png")
-	# c2 = compare(QCD,sig,'jj_l2_tau21',postfix,cut,cut,20,0.,1.,'q candidate #tau_{21}',"","QCD","q*(2 TeV)#rightarrowqW")
+	
+	
+	
+	
+	
+	# c2 = compareSigQCD(QCD,sig,'jj_l2_tau21',postfix,cut,cut,20,0.,1.,'q candidate #tau_{21}',"","QCD","q*(2 TeV)#rightarrowqW")
 	# c2[0].SaveAs(directory+"/"+c2[0].GetName()+".png")
-	# c3 = compare(QCD,sig,'jj_l1_softDrop_mass',postfix,cut,cut,60,0.,300.,'V candidate mass',"GeV","QCD","q*(2 TeV)#rightarrowqW")
+	# c3 = compareSigQCD(QCD,sig,'jj_l1_softDrop_mass',postfix,cut,cut,60,0.,300.,'V candidate mass',"GeV","QCD","q*(2 TeV)#rightarrowqW")
 	# c3[0].SaveAs(directory+"/"+c3[0].GetName()+".png")
-	# c4 = compare(QCD,sig,'jj_l2_softDrop_mass',postfix,cut,cut,60,0.,300.,'q candidate mass',"GeV","QCD","q*(2 TeV)#rightarrowqW")
+	# c4 = compareSigQCD(QCD,sig,'jj_l2_softDrop_mass',postfix,cut,cut,60,0.,300.,'q candidate mass',"GeV","QCD","q*(2 TeV)#rightarrowqW")
 	# c4[0].SaveAs(directory+"/"+c4[0].GetName()+".png")
-	# c5 = compare(QCD,sig,'jj_LV_mass',postfix,cut,cut,80,1000.,8000.,'M_{qV}',"GeV","QCD","q*(2 TeV)#rightarrowqW",1)
+	# c5 = compareSigQCD(QCD,sig,'jj_LV_mass',postfix,cut,cut,80,1000.,8000.,'M_{qV}',"GeV","QCD","q*(2 TeV)#rightarrowqW",1)
 	# c5[0].SaveAs(directory+"/"+c5[0].GetName()+".png")
-	# c6 = compare(QCD,sig,'abs(jj_l1_eta-jj_l2_eta)',postfix,cut,cut,13,0.,1.3,'#Delta#eta(q,V)',"","QCD","q*(2 TeV)#rightarrowqW")
+	# c6 = compareSigQCD(QCD,sig,'abs(jj_l1_eta-jj_l2_eta)',postfix,cut,cut,13,0.,1.3,'#Delta#eta(q,V)',"","QCD","q*(2 TeV)#rightarrowqW")
 	# c6[0].SaveAs(directory+"/"+c6[0].GetName()+".png")
-	# c7 = compare(QCD,sig,'jj_l1_pt',postfix,cut,cut,50,200.,5000.,'V candidate p_{T}',"GeV","QCD","q*(2 TeV)#rightarrowqW",1)
+	# c7 = compareSigQCD(QCD,sig,'jj_l1_pt',postfix,cut,cut,50,200.,5000.,'V candidate p_{T}',"GeV","QCD","q*(2 TeV)#rightarrowqW",1)
 	# c7[0].SaveAs(directory+"/"+c7[0].GetName()+".png")
-	# c8 = compare(QCD,sig,'jj_l2_pt',postfix,cut,cut,50,200.,5000.,'q candidate p_{T}',"GeV","QCD","q*(2 TeV)#rightarrowqW",1)
+	# c8 = compareSigQCD(QCD,sig,'jj_l2_pt',postfix,cut,cut,50,200.,5000.,'q candidate p_{T}',"GeV","QCD","q*(2 TeV)#rightarrowqW",1)
 	# c8[0].SaveAs(directory+"/"+c8[0].GetName()+".png")
 	#
 	
@@ -311,5 +388,6 @@ for p in purities:
 	# d11[0].SaveAs(directory+"/"+d11[0].GetName()+".png")
 	# d12 = compareQCD(QCD,QCDht,QCDherwig,'jj_l2_tau21',postfix,cut,20,0.,1.,'q candidate #tau_{21}','',"Pythia8","Madgraph+Pythia8","Herwig++",0)
 	# d12[0].SaveAs(directory+"/"+d12[0].GetName()+".png")
-	#
+	
+	print "Execution time: " ,datetime.now() - startTime
 
