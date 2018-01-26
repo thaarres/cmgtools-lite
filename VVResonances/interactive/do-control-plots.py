ab7101f29d6c2b892c581631575a85460fc4b61c
Thea Aarrestad <thea.aarrestad@cern.ch>
2018-01-26 11:49:22 +0100

    Newest control plot script

--- VVResonances/interactive/do-control-plots.py
+++ VVResonances/interactive/do-control-plots.py
@@ -32,7 +32,10 @@ W_ref = 800
 W = W_ref
 H  = H_ref
 
-directory='/eos/user/t/thaarres/www/vvana/control_plots'
+directory='control_plots/'
+try: os.stat(directory)
+except: os.mkdir(directory)
+	
 lumi_13TeV = "35.9 fb^{-1}"
 lumi_sqrtS = "13 TeV" # used with iPeriod = 0, e.g. for simulation-only plots (default is an empty string)
 iPeriod=0
@@ -61,19 +64,14 @@ cuts['NP'] = '(('+cat['LP1']+'&&'+cat['NP2']+')||('+cat['NP1']+'&&'+cat['LP2']+'
 cuts['nonres'] = '1'
 
 purities=['HPHP','HPLP','LPLP','NP']
-purities=['HPHP','HPLP']
+purities=['HPHP']
 
-BulkGravWWTemplate="BulkGravToWW_narrow_2000"
-BulkGravZZTemplate="BulkGravToZZToZhadZhad_narrow_2000"
-WprimeTemplate= "WprimeToWZToWhadZhad_narrow_2000"
+
 # use arbitrary cross section 0.001 so limits converge better
 BRWW=1.*0.001
 BRZZ=1.*0.001*0.6991*0.6991
 BRWZ=1.*0.001*0.6991*0.676
 
-dataTemplate="JetHT"
-nonResTemplate="QCD_Pt_" #high stat
-# nonResTemplate="QCD_Pt-" #low stat --> use this for tests
 #nonResTemplate="Dijet" #to compare shapes
 
 minMJ=55.0
@@ -92,35 +90,11 @@ if dijetBinning:
 
 cuts['nonres'] = '1'
 
-
-def getPlotters(samples,isData=False,corr="1"):
-    sampleTypes=samples.split(',')
-    plotters=[]
-    for filename in os.listdir('samples'):
-        for sampleType in sampleTypes:
-            if filename.find(sampleType)!=-1:
-                fnameParts=filename.split('.')
-                fname=fnameParts[0]
-                ext=fnameParts[1]
-                if ext.find("root") ==-1:
-                    continue
-                print 'Adding file',fname
-                plotters.append(TreePlotter('samples/'+fname+'.root','tree'))
-                if not isData:
-                    plotters[-1].setupFromFile('samples/'+fname+'.pck')
-                    plotters[-1].addCorrectionFactor('xsec','tree')
-                    plotters[-1].addCorrectionFactor('genWeight','tree')
-                    plotters[-1].addCorrectionFactor('puWeight','tree')
-                    plotters[-1].addCorrectionFactor(corr,'flat')
-                    
-    return  plotters
-
-def compareSigQCD(p1,p2,var,postfix,cut1,cut2,bins,mini,maxi,title,unit,leg1,leg2,logY=0):
+def getCanvas(name="c1"):
     T = 0.08*H_ref
     B = 0.12*H_ref 
     L = 0.12*W_ref
     R = 0.04*W_ref
-    name = "%s_%s" %(postfix, var.replace("(","").replace(")","").replace("-","_"))
     canvas = ROOT.TCanvas(name,name,50,50,W,H)
     canvas.SetFillColor(0)
     canvas.SetBorderMode(0)
@@ -145,6 +119,35 @@ def compareSigQCD(p1,p2,var,postfix,cut1,cut2,bins,mini,maxi,title,unit,leg1,leg
     legend.SetFillStyle(0)
     legend.SetTextFont(42)
     
+    return canvas,legend
+
+def getPlotters(samples,isData=False,corr="1"):
+    sampleTypes=samples.split(',')
+    plotters=[]
+    for filename in os.listdir('samples'):
+        for sampleType in sampleTypes:
+            if filename.find(sampleType)!=-1:
+                fnameParts=filename.split('.')
+                fname=fnameParts[0]
+                ext=fnameParts[1]
+                if ext.find("root") ==-1:
+                    continue
+                print 'Adding file',fname
+                plotters.append(TreePlotter('samples/'+fname+'.root','tree'))
+                if not isData:
+                    plotters[-1].setupFromFile('samples/'+fname+'.pck')
+                    plotters[-1].addCorrectionFactor('xsec','tree')
+                    plotters[-1].addCorrectionFactor('genWeight','tree')
+                    plotters[-1].addCorrectionFactor('puWeight','tree')
+                    plotters[-1].addCorrectionFactor(corr,'flat')
+                    
+    return  plotters
+
+def compareSigQCD(p1,p2,var,postfix,cut1,cut2,bins,mini,maxi,title,unit,leg1,leg2,logY=0):
+   
+    name = "%s_%s" %(postfix, var.replace("(","").replace(")","").replace("-","_"))
+    canvas,legend = getCanvas(name)
+    canvas.cd()
     h1=p1.drawTH1(var,cut1,lumi,bins,mini,maxi,title,unit,"HIST")
     h2=p2.drawTH1(var,cut2,lumi,bins,mini,maxi,title,unit,"HIST")
     
@@ -182,35 +185,10 @@ def compareSigQCD(p1,p2,var,postfix,cut1,cut2,bins,mini,maxi,title,unit,leg1,leg
     return canvas,h1,h2,legend #,pt
 	 
 def compareQCD(p1,p2,p3,var,postfix,cut,bins,mini,maxi,title,unit,leg1,leg2,leg3,logY=0):
-    T = 0.08*H_ref
-    B = 0.12*H_ref 
-    L = 0.12*W_ref
-    R = 0.04*W_ref
     name = "qcdCompare_%s_%s" %(postfix, var.replace("(","").replace(")","").replace("-","_"))
-    canvas = ROOT.TCanvas(name,name,50,50,W,H)
-    canvas.SetFillColor(0)
-    canvas.SetBorderMode(0)
-    canvas.SetFrameFillStyle(0)
-    canvas.SetFrameBorderMode(0)
-    canvas.SetLeftMargin( L/W )
-    canvas.SetRightMargin( R/W )
-    canvas.SetTopMargin( T/H )
-    canvas.SetBottomMargin( B/H )
-    canvas.SetTickx(0)
-    canvas.SetTicky(0)
-    
-    ROOT.gStyle.SetOptStat(0)
-    ROOT.gStyle.SetOptTitle(0)
+    canvas,legend = getCanvas(name)
     canvas.cd()
-    legend = ROOT.TLegend(0.62,0.7,0.92,0.9,"","brNDC")
-    legend.SetBorderSize(0)
-    legend.SetLineColor(1)
-    legend.SetLineStyle(1)
-    legend.SetLineWidth(1)
-    legend.SetFillColor(0)
-    legend.SetFillStyle(0)
-    legend.SetTextFont(42)
-    
+
     h1=p1.drawTH1(var,cut,lumi,bins,mini,maxi,title,unit,"HIST")
     h2=p2.drawTH1(var,cut,lumi,bins,mini,maxi,title,unit,"HIST")
     h3=p3.drawTH1(var,cut,lumi,bins,mini,maxi,title,unit,"HIST")
@@ -247,59 +225,38 @@ def compareQCD(p1,p2,p3,var,postfix,cut,bins,mini,maxi,title,unit,leg1,leg2,leg3
 
     return canvas,h1,h2,h3,legend #,pt
 
-def dataMC(data,mc,sig,var,postfix,cut,bins,mini,maxi,title,unit,leg1,leg2,leg3,logY=0):
-    T = 0.08*H_ref
-    B = 0.12*H_ref 
-    L = 0.12*W_ref
-    R = 0.04*W_ref
+def dataMC(mc,data,sig1,sig2,sig3,var,postfix,cut,bins,mini,maxi,title,unit,ytitle,legMC,legDat,legSig1,legSig2,legSig3,logY=0):
     name = "%s_%s" %(postfix, var.replace("(","").replace(")","").replace("-","_"))
-    canvas = ROOT.TCanvas(name,name,50,50,W,H)
-    canvas.SetFillColor(0)
-    canvas.SetBorderMode(0)
-    canvas.SetFrameFillStyle(0)
-    canvas.SetFrameBorderMode(0)
-    canvas.SetLeftMargin( L/W )
-    canvas.SetRightMargin( R/W )
-    canvas.SetTopMargin( T/H )
-    canvas.SetBottomMargin( B/H )
-    canvas.SetTickx(0)
-    canvas.SetTicky(0)
+    canvas, legend = getCanvas(name)
+
+    hData = data.drawTH1(var,cut,"1.",bins,mini,maxi,title,unit,"HIST")
+    hMC   = mc  .drawTH1(var,cut,lumi,bins,mini,maxi,title,unit,"HIST")
+    hSig1 = sig1.drawTH1(var,cut,lumi,bins,mini,maxi,title,unit,"HIST"); hSig1.Scale(0.1*(hData.Integral()/hSig1.Integral()))
+    hSig2 = sig2.drawTH1(var,cut,lumi,bins,mini,maxi,title,unit,"HIST"); hSig2.Scale(0.1*(hData.Integral()/hSig2.Integral()))
+    hSig3 = sig3.drawTH1(var,cut,lumi,bins,mini,maxi,title,unit,"HIST"); hSig3.Scale(0.1*(hData.Integral()/hSig3.Integral()))
     
-    ROOT.gStyle.SetOptStat(0)
-    ROOT.gStyle.SetOptTitle(0)
-    canvas.cd()
-    legend = ROOT.TLegend(0.62,0.7,0.92,0.9,"","brNDC")
-    legend.SetBorderSize(0)
-    legend.SetLineColor(1)
-    legend.SetLineStyle(1)
-    legend.SetLineWidth(1)
-    legend.SetFillColor(0)
-    legend.SetFillStyle(0)
-    legend.SetTextFont(42)
-    
-    hData=data.drawTH1(var,cut,lumi,bins,mini,maxi,title,unit,"HIST")
-    hMC=mc.drawTH1(var,cut,lumi,bins,mini,maxi,title,unit,"HIST")
-    hSig=sig.drawTH1(var,cut,lumi,bins,mini,maxi,title,unit,"HIST")
-    hSig.Scale((hMC.Integral()/hSig.Integral()))
-    
+    hData.GetYaxis().SetTitle(ytitle)
+    hData.GetXaxis().SetTitle(hData.GetXaxis().GetTitle().replace("[]",""))
     hData.Draw("EP")
     hMC.Draw("HISTCSAME")
-    hSig.Draw("HISTCSAME")
+    hSig1.Draw("HISTCSAME")
+    hSig2.Draw("HISTCSAME")
+    hSig3.Draw("HISTCSAME")
     maxY = hData.GetMaximum()*1.5
-    if hData.GetMaximum() < hMC.GetMaximum(): 
-		 maxY = hMC.GetMaximum()*1.5
-    if logY: 
-		 canvas.SetLogy()
-		 maxY = maxY*200
+    # if hData.GetMaximum() < hMC.GetMaximum(): maxY = hMC.GetMaximum()*1.5
+ #    if logY: canvas.SetLogy(); maxY = maxY*200
     hData.GetYaxis().SetRangeUser(0.001,maxY)
     # hData.DrawNormalized("HISTC")
     # hMC.DrawNormalized("SAMEHIST")
     # hData=data.drawTH1Binned(var,cut1,lumi,bins,title,unit, "HISTC")
     # hMC=mc.drawTH1Binned(var,cut2,lumi,bins,title,unit, "SAMEHIST")
-    legend.AddEntry(hData,leg1,"LEP")
-    legend.AddEntry(hMC,leg2,"F")
-    legend.AddEntry(hSig,leg3,"F")
+    legend.AddEntry(hData,legDat,"LEP")
+    legend.AddEntry(hMC  ,legMC,"F")
+    legend.AddEntry(hSig1 ,legSig1,"L")
+    legend.AddEntry(hSig2 ,legSig2,"L")
+    legend.AddEntry(hSig3 ,legSig3,"L")
     legend.Draw()
+    
     
     
     # pt =ROOT.TPaveText(0.1577181,0.9562937,0.9580537,0.9947552,"brNDC")
@@ -310,42 +267,60 @@ def dataMC(data,mc,sig,var,postfix,cut,bins,mini,maxi,title,unit,leg1,leg2,leg3,
     # pt.SetTextSize(0.03)
     #
     # pt.Draw()
+    canvas.Update()
     cmslabel_prelim(canvas,'2016',11)
-
+    canvas.Update()
+    return canvas,hData,hSig1,hSig2,hSig3,legend #,pt
     
-    return canvas,hData,hMC,legend #,pt
-	
 
 
 
-# QCDPtPlotters = getPlotters(nonResTemplate,False)
-# QCD = MergedPlotter(QCDPtPlotters)
-# QCD.setFillProperties(1001,921)
 
 # QCDhtPlotters = getPlotters('QCD_HT',False)
 # QCDht = MergedPlotter(QCDhtPlotters)
 # QCDht.setLineProperties(1,633,2)
 # QCDht.setFillProperties(1001,0)
 
+BulkGravWWTemplate="BulkGravToWW_narrow_1800"
+BulkGravZZTemplate="BulkGravToZZToZhadZhad_narrow_1800"
+WprimeTemplate= "WprimeToWZToWhadZhad_narrow_1800"
+dataTemplate="JetHT"
+nonResTemplate="QCD_Pt_" #high stat
+nonResTemplateH="QCD_Pt-" #low stat --> use this for tests
 
+# QCDPtPlotters = getPlotters(nonResTemplate,False)
+# QCD = MergedPlotter(QCDPtPlotters)
+# QCD.setLineProperties(1,1,1)
+# QCD.setFillProperties(3001,36)
 
-
-QCDherwigPlotters = getPlotters('QCD_Pt-',False)
+QCDherwigPlotters = getPlotters(nonResTemplate,False)
 QCDherwig = MergedPlotter(QCDherwigPlotters)
-QCDherwig.setFillProperties(1001,921)
+QCDherwig.setLineProperties(1,1,1)
+QCDherwig.setFillProperties(3001,36)
 
 DATAPlotters = getPlotters(dataTemplate,True)
 data=MergedPlotter(DATAPlotters)
+data.setLineProperties(1,1,1)
+data.setFillProperties(0,0)
 
-SigPlotters = getPlotters(BulkGravWWTemplate,False)
-sig = MergedPlotter(SigPlotters)
+SigPlottersWZ = getPlotters(WprimeTemplate,False)
+sigWZ = MergedPlotter(SigPlottersWZ)
+sigWZ.setLineProperties(1,ROOT.kMagenta+2,3)
+sigWZ.setFillProperties(0,0)
 
-sig.setFillProperties(3001,36)
+SigPlottersWW = getPlotters(BulkGravWWTemplate,False)
+sigWW = MergedPlotter(SigPlottersWW)
+sigWW.setLineProperties(2,ROOT.kOrange+7,3)
+sigWW.setFillProperties(0,0)
 
+SigPlottersZZ = getPlotters(BulkGravZZTemplate,False)
+sigZZ = MergedPlotter(SigPlottersZZ)
+sigZZ.setLineProperties(9,ROOT.kGreen+2,3)
+sigZZ.setFillProperties(0,0)
 
 #Stack
 # jjStack = StackPlotter()
-# jjStack.addPlotter(QCD,"QCD","QCD multijet","background")
+# jjStack.addPlotter(QCD,"QCD Pythia8","QCD multijet","background")
 # jjStack.addPlotter(data,"data_obs","Data","data")
 
 for p in purities:
@@ -353,27 +328,68 @@ for p in purities:
 	print "Plotting variables for category %s" %(p)
 	cut='*'.join([cuts['common'],cuts[p]])
 	postfix = p
-	
-	c1 = dataMC(QCDherwig,QCDherwig,sig,'jj_l1_softDrop_mass',postfix,cut,40,55.,215.,'V candidate softdrop mass',"GeV","QCD","Data","G_{Bulk}(2 TeV)#rightarrowWW")
+	c1 = dataMC(QCDherwig,data,sigWZ,sigWW,sigZZ,'jj_l1_softDrop_mass',postfix,cut,32,55.,215.,'V candidate softdrop mass',"GeV","Events / 5 GeV","QCD Pythia8","Data","W'(2 TeV)#rightarrowWZ","G_{Bulk}(2 TeV)#rightarrowWW","G_{Bulk}(2 TeV)#rightarrowZZ")
 	c1[0].SaveAs(directory+"/"+c1[0].GetName()+".png")
+	c1[0].SaveAs(directory+"/"+c1[0].GetName()+".root")
+	c2 = dataMC(QCDherwig,data,sigWZ,sigWW,sigZZ,'jj_l1_mass',postfix,cut,32,55.,215.,'V candidate bare mass',"GeV","Events / 5 GeV","QCD Pythia8","Data","W'(2 TeV)#rightarrowWZ","G_{Bulk}(2 TeV)#rightarrowWW","G_{Bulk}(2 TeV)#rightarrowZZ")
+	c2[0].SaveAs(directory+"/"+c1[0].GetName()+".png")
+	c2[0].SaveAs(directory+"/"+c1[0].GetName()+".root")
+# 	canv,leg= getCanvas("mass")
+# 	canv.cd()
+# 	c2[1].GetXaxis().SetTitle("V candidate mass [GeV]")
+# 	# c2[2].SetLineStyle(2)
+# # 	c2[3].SetLineStyle(2)
+# # 	c2[1].SetMarkerStyle(4)
+# 	c2[1].Draw("EP")
+# 	c2[2].Draw("CSAME")
+# 	c2[3].Draw("CSAME")
+# 	c1[1].Draw("EPSAME")
+# 	c1[2].Draw("HISTCSAME")
+# 	c1[3].Draw("HISTCSAME")
+# 	leg.AddEntry(c1[1],"Data","LEP")
+# 	leg.AddEntry(c1[2],"QCD Pythia8","F")
+# 	leg.AddEntry(c1[3],"W'(2 TeV)#rightarrowWZ","L")
+# 	leg.Draw()
+# 	canv.SaveAs(directory+"/mass_compare.png")
+# 	canv.SaveAs(directory+"/mass_compare.root")
+postfix = "looseSel"
+cut='*'.join([cuts['common']])
+c1 = dataMC(QCDherwig,data,sigWZ,sigWW,sigZZ,'jj_l1_pt',postfix,cut,48,200.,5000.,'V candidate p_{T}',"GeV","Events / 100 GeV","QCD Pythia8","Data","W'(2 TeV)#rightarrowWZ","G_{Bulk}(2 TeV)#rightarrowWW","G_{Bulk}(2 TeV)#rightarrowZZ")
+c1[0].SaveAs(directory+"/"+c1[0].GetName()+".png")
+c1[0].SaveAs(directory+"/"+c1[0].GetName()+".root")
+c1 = dataMC(QCDherwig,data,sigWZ,sigWW,sigZZ,'jj_l1_eta',postfix,cut,25,-2.5,2.5,'V candidate #eta',"","Events / 0.2","QCD Pythia8","Data","W'(2 TeV)#rightarrowWZ","G_{Bulk}(2 TeV)#rightarrowWW","G_{Bulk}(2 TeV)#rightarrowZZ")
+c1[0].SaveAs(directory+"/"+c1[0].GetName()+".png")
+c1[0].SaveAs(directory+"/"+c1[0].GetName()+".root")
+c1 = dataMC(QCDherwig,data,sigWZ,sigWW,sigZZ,'jj_LV_mass',postfix,cut,40,1000.,5000.,'Dijet invariant mass',"GeV","Events / 100 GeV","QCD Pythia8","Data","W'(2 TeV)#rightarrowWZ","G_{Bulk}(2 TeV)#rightarrowWW","G_{Bulk}(2 TeV)#rightarrowZZ",1)
+c1[0].SaveAs(directory+"/"+c1[0].GetName()+".png")
+c1[0].SaveAs(directory+"/"+c1[0].GetName()+".root")
+cut='*'.join([cuts['common'],"(jj_l1_softDrop_mass>{minMJ}&&jj_l1_softDrop_mass<{maxMJ})".format(minMJ=minMJ,maxMJ=maxMJ)])	
+c1 = dataMC(QCDherwig,data,sigWZ,sigWW,sigZZ,'jj_l1_tau2/jj_l1_tau1',postfix,cut,20,0.,1.,'V candidate #tau_{21}',"","Events / 0.05","QCD Pythia8","Data","W'(2 TeV)#rightarrowWZ","G_{Bulk}(2 TeV)#rightarrowWW","G_{Bulk}(2 TeV)#rightarrowZZ")
+c1[0].SaveAs(directory+"/"+c1[0].GetName()+".png")
+c1[0].SaveAs(directory+"/"+c1[0].GetName()+".root")	
+c1 = dataMC(QCDherwig,data,sigWZ,sigWW,sigZZ,'(jj_l1_tau2/jj_l1_tau1+(0.082*TMath::Log((jj_l1_softDrop_mass*jj_l1_softDrop_mass)/jj_l1_pt)))',postfix,cut,24,0.,1.2,'V candidate #tau_{21}^{DDT}','',"Events / 0.05","QCD Pythia8","Data","W'(2 TeV)#rightarrowWZ","G_{Bulk}(2 TeV)#rightarrowWW","G_{Bulk}(2 TeV)#rightarrowZZ")
+c1[0].SaveAs(directory+"/ddt_l1.png")
+c1[0].SaveAs(directory+"/ddt_l1.root")	
+c1 = dataMC(QCDherwig,data,sigWZ,sigWW,sigZZ,'(jj_l2_tau2/jj_l2_tau1+(0.082*TMath::Log((jj_l2_softDrop_mass*jj_l2_softDrop_mass)/jj_l2_pt)))',postfix,cut,24,0.,1.2,'V candidate #tau_{21}^{DDT}','',"Events / 0.05","QCD Pythia8","Data","W'(2 TeV)#rightarrowWZ","G_{Bulk}(2 TeV)#rightarrowWW","G_{Bulk}(2 TeV)#rightarrowZZ")
+c1[0].SaveAs(directory+"/ddt_l2.png")
+c1[0].SaveAs(directory+"/ddt_l2.root")	
 	
 	
 	
 	
-	
-	# c2 = compareSigQCD(QCD,sig,'jj_l2_tau21',postfix,cut,cut,20,0.,1.,'q candidate #tau_{21}',"","QCD","q*(2 TeV)#rightarrowqW")
+	# c2 = compareSigQCD(QCD,sig,'jj_l2_tau21',postfix,cut,cut,20,0.,1.,'q candidate #tau_{21}',"","QCD Pythia8","q*(2 TeV)#rightarrowqW")
 	# c2[0].SaveAs(directory+"/"+c2[0].GetName()+".png")
-	# c3 = compareSigQCD(QCD,sig,'jj_l1_softDrop_mass',postfix,cut,cut,60,0.,300.,'V candidate mass',"GeV","QCD","q*(2 TeV)#rightarrowqW")
+	# c3 = compareSigQCD(QCD,sig,'jj_l1_softDrop_mass',postfix,cut,cut,60,0.,300.,'V candidate mass',"GeV","QCD Pythia8","q*(2 TeV)#rightarrowqW")
 	# c3[0].SaveAs(directory+"/"+c3[0].GetName()+".png")
-	# c4 = compareSigQCD(QCD,sig,'jj_l2_softDrop_mass',postfix,cut,cut,60,0.,300.,'q candidate mass',"GeV","QCD","q*(2 TeV)#rightarrowqW")
+	# c4 = compareSigQCD(QCD,sig,'jj_l2_softDrop_mass',postfix,cut,cut,60,0.,300.,'q candidate mass',"GeV","QCD Pythia8","q*(2 TeV)#rightarrowqW")
 	# c4[0].SaveAs(directory+"/"+c4[0].GetName()+".png")
-	# c5 = compareSigQCD(QCD,sig,'jj_LV_mass',postfix,cut,cut,80,1000.,8000.,'M_{qV}',"GeV","QCD","q*(2 TeV)#rightarrowqW",1)
+	# c5 = compareSigQCD(QCD,sig,'jj_LV_mass',postfix,cut,cut,80,1000.,8000.,'M_{qV}',"GeV","QCD Pythia8","q*(2 TeV)#rightarrowqW",1)
 	# c5[0].SaveAs(directory+"/"+c5[0].GetName()+".png")
-	# c6 = compareSigQCD(QCD,sig,'abs(jj_l1_eta-jj_l2_eta)',postfix,cut,cut,13,0.,1.3,'#Delta#eta(q,V)',"","QCD","q*(2 TeV)#rightarrowqW")
+	# c6 = compareSigQCD(QCD,sig,'abs(jj_l1_eta-jj_l2_eta)',postfix,cut,cut,13,0.,1.3,'#Delta#eta(q,V)',"","QCD Pythia8","q*(2 TeV)#rightarrowqW")
 	# c6[0].SaveAs(directory+"/"+c6[0].GetName()+".png")
-	# c7 = compareSigQCD(QCD,sig,'jj_l1_pt',postfix,cut,cut,50,200.,5000.,'V candidate p_{T}',"GeV","QCD","q*(2 TeV)#rightarrowqW",1)
+	# c7 = compareSigQCD(QCD,sig,'jj_l1_pt',postfix,cut,cut,50,200.,5000.,'V candidate p_{T}',"GeV","QCD Pythia8","q*(2 TeV)#rightarrowqW",1)
 	# c7[0].SaveAs(directory+"/"+c7[0].GetName()+".png")
-	# c8 = compareSigQCD(QCD,sig,'jj_l2_pt',postfix,cut,cut,50,200.,5000.,'q candidate p_{T}',"GeV","QCD","q*(2 TeV)#rightarrowqW",1)
+	# c8 = compareSigQCD(QCD,sig,'jj_l2_pt',postfix,cut,cut,50,200.,5000.,'q candidate p_{T}',"GeV","QCD Pythia8","q*(2 TeV)#rightarrowqW",1)
 	# c8[0].SaveAs(directory+"/"+c8[0].GetName()+".png")
 	#
 	
@@ -389,5 +405,5 @@ for p in purities:
 	# d12 = compareQCD(QCD,QCDht,QCDherwig,'jj_l2_tau21',postfix,cut,20,0.,1.,'q candidate #tau_{21}','',"Pythia8","Madgraph+Pythia8","Herwig++",0)
 	# d12[0].SaveAs(directory+"/"+d12[0].GetName()+".png")
 	
-	print "Execution time: " ,datetime.now() - startTime
+print "Execution time: " ,datetime.now() - startTime
 
