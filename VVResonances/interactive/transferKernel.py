50a542958925e506dc6476fd2cc9e0f553ae5b93
IreneZoi <irene.zoi@cern.ch>
2019-12-04 17:48:16 +0100

    fixing merging con flicts

--- VVResonances/interactive/transferKernel.py
+++ VVResonances/interactive/transferKernel.py
@@ -382,7 +382,8 @@ def makeNonResCard():
  else: category_pdf = "VV_LPLP"  
 
  dataset = options.year
- sig = 'BulkGWW'
+ sig = 'BulkGWW' 
+ doCorrelation = False
  if 'VBF' in purity: sig = 'VBF_BulkGWW'
  
  lumi = {'2016':35900,'2017':41367}
@@ -402,8 +403,10 @@ def makeNonResCard():
  vtag_unc['VH_HPLP'] = {'2016':'0.882/1.12','2017':'0.866/1.136'}    
  vtag_unc['VH_LPHP'] = {'2016':'1.063','2017':'1.043'}
 
- 
- DTools = DatacardTools(scales,scalesHiggs,vtag_pt_dependence,lumi_unc,vtag_unc,1.0,"","",False)
+ vtag_pt_dependence = {'VV_HPHP':'((1+0.06*log(MH/2/300))*(1+0.06*log(MH/2/300)))','VV_HPLP':'((1+0.06*log(MH/2/300))*(1+0.07*log(MH/2/300)))',
+                       'VBF_VV_HPHP':'((1+0.06*log(MH/2/300))*(1+0.06*log(MH/2/300)))','VBF_VV_HPLP':'((1+0.06*log(MH/2/300))*(1+0.07*log(MH/2/300)))'}
+
+ DTools = DatacardTools(scales,scalesHiggs,vtag_pt_dependence,lumi_unc,vtag_unc,1.0,"","",doCorrelation)
  print '##########      PURITY      :', purity 
 
  cat='_'.join(['JJ',sig,purity,'13TeV_'+dataset])
